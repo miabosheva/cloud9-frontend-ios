553d317e41d0d7519eec4be6b824fef270466adc
Mia Bosheva <boshevam@gmail.com>
2025-09-14 13:36:34 +0200

    firebase wip

---
+++ CloudNine/Error Handling/SleepServiceError.swift
@@ -0,0 +1,8 @@
+//
+//  SleepServiceError.swift
+//  CloudNine
+//
+//  Created by Mia Bosheva on 13.9.25.
+//
+
+import Foundation
---
+++ CloudNine/Managers/FirebaseManager.swift
@@ -0,0 +1,8 @@
+//
+//  FirebaseManager.swift
+//  CloudNine
+//
+//  Created by Mia Bosheva on 14.9.25.
+//
+
+import Foundation
